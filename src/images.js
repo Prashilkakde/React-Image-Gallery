ca69f0705cbe86b9d8dff1287a927b811b8e2817
Prashilkakde <96301089+Prashilkakde@users.noreply.github.com>
2022-02-03 18:24:41 +0530

    Initial commit

---
+++ src/images.js
@@ -0,0 +1,12 @@
+export default [
+  "https://images.unsplash.com/photo-1506368249639-73a05d6f6488?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Y29va2luZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+  "https://images.unsplash.com/photo-1518737003272-dac7c4760d5e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y29va2luZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
+  "https://images.unsplash.com/photo-1540420828642-fca2c5c18abe?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTh8fGNvb2tpbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+  "https://images.unsplash.com/photo-1589786742305-f24d19eedbe5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8c2Ftb3NhfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
+  "https://media.istockphoto.com/photos/jalebi-sweet-food-picture-id851334308?b=1&k=20&m=851334308&s=170667a&w=0&h=rU1DID4pBTjDuut5CgnIap15jDRvtVUSvUnULf_kzBo=",
+  "https://media.istockphoto.com/photos/green-peas-or-matar-paneer-curry-picture-id1325271813?b=1&k=20&m=1325271813&s=170667a&w=0&h=43ivkRCQwIq3IFACs2IO7SRC4WKtEBhQ1X-CgAP9Ss4=",
+  "https://media.istockphoto.com/photos/fruit-juice-picture-id155376375?b=1&k=20&m=155376375&s=170667a&w=0&h=KL3cT2jGl9IR6pe9ONF2jgp4ra1E02U3avVm8NIpNm4=",
+  "https://images.unsplash.com/photo-1534353473418-4cfa6c56fd38?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8anVpY2V8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+  "https://media.istockphoto.com/photos/cottage-cheese-cutlet-picture-id1272417152?b=1&k=20&m=1272417152&s=170667a&w=0&h=vaGP0dNUhwz__Qm6I2tmYRnWpP61TWQ7OWqHj4M5KbM=",
+  "https://media.istockphoto.com/photos/paneer-do-pyaza-is-a-popular-punjabi-vegetarian-recipe-using-cubes-of-picture-id1085147262?b=1&k=20&m=1085147262&s=170667a&w=0&h=3hcZAXpcf4l-p5WMinUUTblxwwFcC6vUzewwaxgs6fY="
+];
